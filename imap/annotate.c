d8ea80d464546ab128ceab11e6ef45503cecbc21
Ken Murchison <murch@fastmail.com>
2019-01-10 23:03:14 -0500

    annotate.c: a NULL mailbox argument to annotate_msg_lookup[mask]() should map to a server annotation

--- imap/annotate.c
+++ imap/annotate.c
@@ -2689,7 +2689,8 @@ EXPORTED int annotatemore_msg_lookup(const struct mailbox *mailbox,
                                      uint32_t uid, const char *entry,
                                      const char *userid, struct buf *value)
 {
-    return _annotate_lookup(mailbox->name, mailbox->uniqueid,
+    return _annotate_lookup(mailbox ? mailbox->name : "",
+                            mailbox ? mailbox->uniqueid : NULL,
                             uid, entry, userid, value);
 }
 
@@ -2697,7 +2698,8 @@ EXPORTED int annotatemore_msg_lookupmask(const struct mailbox *mailbox,
                                          uint32_t uid, const char *entry,
                                          const char *userid, struct buf *value)
 {
-    return _annotate_lookupmask(mailbox->name, mailbox->uniqueid,
+    return _annotate_lookupmask(mailbox ? mailbox->name : "",
+                                mailbox ? mailbox->uniqueid : NULL,
                                 uid, entry, userid, value);
 }
 
